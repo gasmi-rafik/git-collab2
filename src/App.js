088fc7fb00e3aea48bc439bcf56a997239c8082b
rafik gasmi <gasmi.rafik.rg@gmail.com>
2021-03-25 12:22:19 +0100

    rafik

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <form onSubmit >
+      {/* <form onSubmit >
         <label style={{color : "green" }}>name :</label>
         <input  type ="text"  placeholder="enter your name"  id="1" value=""/>
         <label style={{ color : "red"}}> last name :</label>
         <input type ="text" placeholder=" enter your last name" id="2" value=""/>
 
         <button style={{backgroundColor : "yellow"}}> submite </button>
-      </form>
+      </form> */}
       hello rafik
 
       second greeting from rafik 
+
       </header>
       <Fras />
     </div>
